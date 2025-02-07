14264d831b52540b1d31651154cda7e77df9a229
abu-huda81 <abuhuda51020@gmail.com>
2025-02-08 00:11:23 +0300

    j

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -8,3 +8,8 @@ router.post('/signin', signin)
 router.post('/signout', signout)
 
 export default router
+// PORT=5000
+
+// MONGO_URI=mongodb+srv://abuhuda51020:DH8ZwGDuC3y2TGhr@cluster0.6ksbi.mongodb.net/auth-tuturial?retryWrites=true&w=majority&appName=Cluster0
+// JWT_SECRET=1Db5B42fgdksodmkvonemlaGfkirf58rekfjvld
+// NODE_ENV=develpment
